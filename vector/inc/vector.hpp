7c694acaec725730d16499e9173c81f447cde493
Tony Josi <tonyjosinew@gmail.com>
2020-09-08 00:01:11 +0530

    header added

---
+++ vector/inc/vector.hpp
@@ -0,0 +1,22 @@
+
+/** 
+ *  @file   vector.ghpp
+ *  @brief  Vector implementation header.
+ *
+ *  This file contains the Header Code for the vector Implementation.
+ *
+ *  @author         Tony Josi   https://tonyjosi97.github.io/profile/
+ *  @copyright      Copyright (C) 2020 Tony Josi
+ *  @bug            No known bugs.
+ */
+
+#ifndef __VECT_IMPL__HEADER__
+#define __VECT_IMPL__HEADER__
+
+namespace rtw_vect {
+
+    
+
+}
+
+#endif /* __VECT_IMPL__HEADER__ */
