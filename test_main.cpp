264f8251cbc89bebdac2237eb6fc94284da6f799
Tony Josi <tonyjosinew@gmail.com>
2020-09-10 23:34:38 +0530

    added tests for validating i/p elements

--- test_main.cpp
+++ test_main.cpp
@@ -1,3 +1,4 @@
+#include <iostream>
 #include "vector/inc/vector.hpp"
 
 using namespace rtw_vect;
@@ -6,10 +7,13 @@ int main() {
 
     vector<int> vect_int;
     for(int i = 0; i < 19; ++i) 
-        vect_int.push_back(23);
+        vect_int.push_back(23 * (i + 1));
 
     vector<int> vect_int_copy(vect_int);
     vector<int> vect_int_copy_assign;
     vect_int_copy_assign = vect_int_copy;
-    
+
+    for(int i = 0; i < 19; ++i) 
+        std::cout<<i<<".    "<<vect_int_copy[i]<<"\n";
+
 }
